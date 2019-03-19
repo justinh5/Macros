89b6da73da2db44569f03b73e797e4464e8536a2
Justin Haupt <jhaupt100@gmail.com>
2019-03-19 09:24:35 -0700

    add meals table header with icons

--- src/app/meal-detail/meal-detail.component.ts
+++ src/app/meal-detail/meal-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-meal-detail',
   templateUrl: './meal-detail.component.html',
-  styleUrls: ['./meal-detail.component.css']
+  styleUrls: ['./meal-detail.component.scss']
 })
 export class MealDetailComponent implements OnInit {
 
